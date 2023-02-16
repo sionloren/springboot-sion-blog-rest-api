666062553888c909a99fe85184a807b45dda8e98
Loren Sion <sion.loren@gmail.com>
2023-02-16 16:17:01 +0100

    Initial commit

---
+++ src/main/java/com/springboot/sion/blog/SpringbootSionBlogRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.sion.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSionBlogRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSionBlogRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/sion/blog/SpringbootSionBlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.sion.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSionBlogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
